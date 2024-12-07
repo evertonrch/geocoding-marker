7d342c2fe2a7a28ba2d9b0496f9844ad355ec94e
Everton Rocha <imevertonrch@gmail.com>
2024-12-07 17:31:10 -0300

    testando chamada e parametrizando a API

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,33 @@
+import axios from "axios"
+
 const form = document.querySelector("form") as HTMLFormElement
 const addressInput = document.querySelector("#address") as HTMLInputElement
+
+interface NominatimGeocoding {
+    lat: string;
+    lon: string;
+    display_name: string;
+    [key: string]: string | number; // Outros campos opcionais
+}
 
 const searchAddressHandler = (e: Event) => {
     e.preventDefault()
 
     const enteredAddress = addressInput.value
 
-    if(!enteredAddress) {
+    if (!enteredAddress) {
         alert("please, enter a address")
         return
     }
 
-    console.log(enteredAddress) 
+    axios
+        .get<NominatimGeocoding[]>(`https://nominatim.openstreetmap.org/search?q=${encodeURI(enteredAddress)}&format=json`)
+        .then(response => {
+            response.data.forEach(place => {
+                console.log(place.type)
+            })
+        })
+        .catch(err => console.error(err))
 }
 
 form.addEventListener("submit", searchAddressHandler)
